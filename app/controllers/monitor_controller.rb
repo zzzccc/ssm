1f6890a770b07accc66a16fe4830e7cec7a509a7
kimsk <kimsk@xubuntu.(none)>
2014-03-19 18:24:46 +0900

    edit map reduce query

--- app/controllers/monitor_controller.rb
+++ app/controllers/monitor_controller.rb
@@ -18,9 +18,40 @@ class MonitorController < ApplicationController
     secs=5
     sec_ago=secs.seconds.ago.iso8601
     now=Time.now.iso8601
-    @count_4XX = Http.where( :code => /^4/, :time => { :$gte => sec_ago , :$lte => now } ).in( port: "#{port}", server: ["#{hostname}"] ).count
-    @count_23X = Http.where( :code => /^2|^3/, :time => { :$gte => sec_ago , :$lte => now } ).in( port: "#{port}", server: ["#{hostname}"] ).count
-    @count_5XX = Http.where( :code => /^5/, :time => { :$gte => sec_ago , :$lte => now } ).in( port: "#{port}", server: ["#{hostname}"] ).count
+
+    map = %Q!
+      function() {
+        var http2xx = new RegExp(/^2|^3/);
+        var http4xx = new RegExp(/^4/);
+        var http5xx = new RegExp(/^5/);
+        if(http2xx.test(this.code)) emit("2XX", {count:1});
+        if(http4xx.test(this.code)) emit("4XX", {count:1});
+        if(http5xx.test(this.code)) emit("5XX", {count:1});
+      }
+    !
+
+
+    reduce = %Q!
+      function(key, values) {    
+        var result = { total:0};
+        result.total = values.length;
+        return result;
+      }
+    !
+
+    result=Http.where( :time => { :$gte => sec_ago , :$lte => now } ,port: "#{port}", server: "#{hostname}" ).map_reduce(map,reduce).out(inline:true)
+
+    @count_4XX,@count_23X,@count_5XX = 0,0,0
+    result.each do |res|
+      @count_23X=res["value"]["total"].to_i if "2XX"==res["_id"]
+      @count_4XX=res["value"]["total"].to_i if "4XX"==res["_id"]
+      @count_5XX=res["value"]["total"].to_i if "5XX"==res["_id"]
+    end
+
+
+    #@count_4XX = Http.where( :code => /^4/, :time => { :$gte => sec_ago , :$lte => now } ,port: "#{port}", server: "#{hostname}").count
+    #@count_23X = Http.where( :code => /^2|^3/, :time => { :$gte => sec_ago , :$lte => now } ,port: "#{port}", server: "#{hostname}").count
+    #@count_5XX = Http.where( :code => /^5/, :time => { :$gte => sec_ago , :$lte => now } ,port: "#{port}", server: "#{hostname}").count
   end
 
   def edit
